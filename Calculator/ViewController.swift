70bcd88fb93473cf11dc6bb7b03a6589540dbcf5
Sa Myeonggu <quest4i@me.com>
2018-05-08 19:26:30 +0900

    finish chapter 1

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 사명구 on 2018. 5. 8..
+//  Copyright © 2018 Learn iOS. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var display: UILabel!
+
+    var userIsInTheMiddleOfTyping = false
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                display.text = String(Double.pi)
+            }
+        }
+    }
+    
+//    override func viewDidLoad() {
+//    super.viewDidLoad()
+//    // Do any additional setup after loading the view, typically from a nib.
+//    }
+//
+//
+//    override func didReceiveMemoryWarning() {
+//    super.didReceiveMemoryWarning()
+//    // Dispose of any resources that can be recreated.
+//    }
+
+
+
+}
