620ce04c1ebd46d9b84dbad75c30b06940518d7f
Sa Myeonggu <quest4i@me.com>
2018-05-13 00:02:57 +0900

    계산기 완성

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -6,24 +6,27 @@
 import Foundation
 
 
-func multiply(op1: Double, op2: Double) -> Double {
-    return op1 * op2
-}
-
 class CalculatorBrain {
 
     private var accumulator: Double = 0.0
 
-    var result: Double {
-        get {
-            return accumulator
-        }
-    }
-
-
     func setOperand(operand: Double) {
         accumulator = operand
     }
+
+
+    private var operations: Dictionary<String, Operation> = [
+        "π": Operation.Constant(Double.pi),
+        "e": Operation.Constant(M_E),
+        "±": Operation.UnaryOperation({ -$0 }),
+        "√": Operation.UnaryOperation(sqrt),
+        "cos": Operation.UnaryOperation(cos),
+        "×": Operation.BinaryOperation({ $0 * $1 }),
+        "÷": Operation.BinaryOperation({ $0 / $1 }),
+        "+": Operation.BinaryOperation({ $0 + $1 }),
+        "−": Operation.BinaryOperation({ $0 - $1 }),
+        "=": Operation.Equals
+    ]
 
 
     private enum Operation {
@@ -33,32 +36,40 @@ class CalculatorBrain {
         case Equals
     }
 
-    private var pending: PendingBinaryOperationInfo
+    func performOperation(symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(
+                        binaryOperation: function, firstOperand: accumulator)
+            case .Equals:
+                executePendingBinaryOperation()
+            }
+        }
+    }
+
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryOperation(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+
+    private var pending: PendingBinaryOperationInfo?
 
     private struct PendingBinaryOperationInfo {
         var binaryOperation: (Double, Double) -> Double
         var firstOperand: Double
     }
 
-    private var operations: Dictionary<String, Operation> = [
-        "π": Operation.Constant(Double.pi),
-        "e": Operation.Constant(M_E),
-        "√": Operation.UnaryOperation(sqrt),
-        "cos": Operation.UnaryOperation(cos),
-        "×": Operation.BinaryOperation(multiply),
-        "=": Operation.Equals
-    ]
-
-    func performOperation(symbol: String) {
-        if let operation = operations[symbol] {
-            switch operation {
-            case .Constant(let value): accumulator = value
-            case .UnaryOperation(let function): accumulator = function(accumulator)
-            case .BinaryOperation(let function): pending = PendingBinaryOperationInfo(
-                    binaryOperation: function, firstOperand: accumulator)
-            case .Equals: break
-            }
+    var result: Double {
+        get {
+            return accumulator
         }
     }
-
 }
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,6 +8,17 @@
 
 import UIKit
 
+/*
+To Do
+1. .으로 시작하는 경우 0.으로 변환
+2. ..으로 입력할 경우 두번째 .은 무시한다.
+3. 00으로 입력할 경우 두번째 0은 무시한다.
+4. 이미 0.000.00 같은 경우는 .을 무시한다.
+
+
+5. display.text는 항상 double형태를 유지해야 한다.
+*/
+
 
 class ViewController: UIViewController {
 
@@ -31,9 +42,21 @@ class ViewController: UIViewController {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTyping {
             let textCurrentlyInDisplay = display.text!
+            if textCurrentlyInDisplay == "0" && digit == "0" {
+                return
+            }
+            if textCurrentlyInDisplay == "." && digit == "." {
+                return
+            }
+            if textCurrentlyInDisplay.contains(".") && digit == "."{
+                return
+            }
             display.text = textCurrentlyInDisplay + digit
         } else {
             display.text = digit
+            if display.text == "." {
+                display.text = "0."
+            }
         }
         userIsInTheMiddleOfTyping = true
     }
